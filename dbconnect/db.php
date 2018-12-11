a669ad3f682462f28b6fbca95350aefe5254b57f
joshcangit <33022160+joshcangit@users.noreply.github.com>
2018-12-11 16:20:40 +0000

    more comments

--- dbconnect/db.php
+++ dbconnect/db.php
@@ -1,10 +1,10 @@
 <?php
 class DB {
-	private $host = "localhost"; //Check your MySQL configuration. It may be different.
-	private $user = "root"; //Should be different if database is owned by another user.
-	private $password = ""; //Password is blank by default on XAMPP.
-	private $database = ""; //Allows you to perform queries without `database.table` format.
-
+	private $host = "localhost"; // Check your MySQL configuration. It may be different.
+	private $user = "root"; // Should be different if database is owned by another user.
+	private $password = ""; // Password is blank by default on XAMPP.
+	private $database = ""; // If specified, allows you to perform queries without `database.table` format.
+                            // However, you can only perform queries on that database which is specified.
 	function __construct() {
 		$this->mysqli = $this->connect();
 	}
@@ -13,7 +13,7 @@ class DB {
 		ini_set('display_errors',1); // Turn on displaying errors
 		error_reporting(E_ALL); // Set error level
 		mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // Set report functions
-		return new mysqli($this->host,$this->user,$this->password,$this->database);
+		return new mysqli($this->host,$this->user,$this->password,$this->database); // Database connection
 	}
 
     function mysqli($sql, $params = array(), $types = "") {
@@ -23,7 +23,7 @@ class DB {
             $stmt = $this->mysqli->prepare($sql);
             if (!$types) $types = $types ?: str_repeat("s", count($params)); // Default to string if undefined
             if (version_compare(PHP_VERSION, '5.6') >= 0) {
-                $stmt->bind_param($types, ...$params); //bind_param using spread operator
+                $stmt->bind_param($types, ...$params); // bind_param using spread operator
             } else {
                 $bind_array = array($types);
                 foreach ($params as $key => $value) {
